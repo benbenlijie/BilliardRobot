83616b772f4c0389abad78de938de1ed491207af
Zhaoyang <zhaoyangliu10@gmail.com>
2022-03-26 17:51:12 +0800

    fix: fix angle calculation

--- PlanningCore/core/__init__.py
+++ PlanningCore/core/__init__.py
@@ -9,11 +9,15 @@ def get_demo_table():
     from itertools import product
 
     from PlanningCore.billiard import Ball, Table, Pocket
-    cue_ball = Ball(no=0, color='white', pos=(table_width / 2, 0.33), radius=ball_radius, is_cue=True)
+    cue_ball = Ball(no=0, color='white', pos=(table_width / 2, 5.33), radius=ball_radius, is_cue=True)
     ball7 = Ball(no=7, color='blue', pos=(table_width/2 - table_width/5, 0.91), radius=ball_radius)
-    ball3 = Ball(no=3, color='yellow', pos=(table_width/2 + table_width/6 + 0.2, 2.2), radius=ball_radius)
+    ball3 = Ball(no=3, color='yellow', pos=(2.9, 4.3), radius=ball_radius)
     ball9 = Ball(no=9, color='black', pos=(table_width / 2 - 1, 3.66), radius=ball_radius)
-    balls = [cue_ball, ball3, ball9]
+    balls = [
+        cue_ball,
+        # ball9,
+        ball3,
+    ]
     pockets = [Pocket(no=i, pos=(x, y), radius=pocket_radius)
                for i, (x, y) in enumerate(product((0, table_width), (0, table_height/2, table_height)))]
     table = Table(width=table_width, height=table_height, balls=balls, pockets=pockets)
@@ -75,16 +79,24 @@ def animate(pockets, logs, flip=False):
 
 if __name__ == '__main__':
     from PlanningCore.billiard import init_table
-    t = init_table((0,0),[(1,1)])
-    # angles = search_optimal_strike(table=t, dt=0.02, dang=0.5)
-    # print(angles)
-    # shot(
-    #     table=t,
-    #     v_cue=1,
-    #     phi=angles[-1][1],
-    #     theta=0,
-    #     a=0,
-    #     b=0,
-    # )
-    # simulate(t, dt=0.02, log=True)
+    # t = get_demo_table()
+    cue_ball_pos = [-0.9061838325810423, -2.8713336068937214]
+    balls_pos = [
+        [-1.2748009848173985, -3.2496512105047186],
+        [0.3936766516208451, -2.8422322527697985],
+        [0.810796060730406, -2.8907345096430035],
+        [0.8544480919162902, -2.560919162905211],
+    ]
+    t = init_table(cue_ball_pos, balls_pos)
+    angles = search_optimal_strike(table=t, dt=0.02, dang=0.5)
+    print(angles)
+    shot(
+        table=t,
+        v_cue=1,
+        phi=angles[-1][1],
+        theta=0,
+        a=0,
+        b=0,
+    )
+    simulate(t, dt=0.02, log=True)
     animate(t.pockets, t.log, False)
--- PlanningCore/core/planning.py
+++ PlanningCore/core/planning.py
@@ -1,3 +1,5 @@
+from math import degrees, sqrt, acos
+
 import numpy as np
 
 from PlanningCore.core.utils import get_common_tangent_angles
@@ -23,4 +25,14 @@ def search_optimal_strike(table, dt, dang):
                     if ball.state == State.pocketed:
                         angles.append((ball.no, ang))
             table.reset_balls()
+    if not angles:
+        cue_ball = table.balls[0]
+        target_ball = table.balls[1]
+        r1 = np.asarray(cue_ball.pos)
+        r2 = np.asarray(target_ball.pos)
+        len_r1_r2 = sqrt(((r1 - r2) ** 2).sum())
+        angle = degrees(acos((r2[0]-r1[0]) / len_r1_r2))
+        if r2[1] < r1[1]:
+            angle = -angle
+        angles.append((target_ball.no, angle))
     return angles
